d6945b0fbdc4a506fe49a3512dfe5f428a9dca6a
kimchaneil <chaneil2@naver.com>
2023-07-12 01:40:27 +0900

    [ADD] express_log2

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 function logger(req,res,next){
   console.log('i am logger')
+  next(); // next 함수가 없다면 다음 logger2가 작동하지 않음
+}
+function logger2(req,res,next){
+  console.log('i am logger 2')
   next();
 }
 app.use(logger);
+app.use(logger2);
 app.listen(3000,function(){
   console.log('Server is running');
 })
