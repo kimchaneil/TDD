262bf257d9c145a31ea12f645feed9d8c0eeb3dd
kimchaneil <chaneil2@naver.com>
2023-07-12 04:34:29 +0900

    [ADD] mocha

---
+++ utils.js
@@ -0,0 +1,8 @@
+function capitialize (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+module.exports = {
+    capitialize : capitialize
+}
+
---
+++ utils.spec.js
@@ -0,0 +1,10 @@
+const { describe } = require("node:test");
+const utils = require('./utils');
+const assert = require('assert');
+describe ('utils.js 모듈의 capitialize()함수는',()=>{
+    it('문자열의 첫번째 문자를  대문자로 변환한다.',()=>{
+        const result = utils.capitialize('hello')
+        assert.equal(result,'Hello');
+    })
+
+})
