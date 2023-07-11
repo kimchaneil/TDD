9084213648bb2f33247afa78a996ff8bedbd1584
kimchaneil <chaneil2@naver.com>
2023-07-12 04:37:43 +0900

    [ADD] should

--- utils.spec.js
+++ utils.spec.js
@@ -1,10 +1,10 @@
 const { describe } = require("node:test");
 const utils = require('./utils');
-const assert = require('assert');
+const should = require('should'); // 가독성을 높이기 위함
 describe ('utils.js 모듈의 capitialize()함수는',()=>{
     it('문자열의 첫번째 문자를  대문자로 변환한다.',()=>{
-        const result = utils.capitialize('hello')
-        assert.equal(result,'Hello');
+        const result = utils.capitialize('hello');
+        result.should.be.equal('Hello');
     })
 
 })
