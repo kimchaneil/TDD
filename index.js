7f7d2787ab0259bc86cd0bc4e527b5101ad58832
kimchaneil <chaneil2@naver.com>
2023-07-12 01:35:48 +0900

    [ADD] express_app

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-
-  
-  if (req.url== '/'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-  }else if (req.url=='/users') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('User List');
-  }else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000,function(){
+  console.log('Server is running');
+})
