27cf042a1da8255e5f31717fb2333031eeae2863
kimchaneil <chaneil2@naver.com>
2023-07-12 00:59:32 +0900

    [ADD]OpenServer

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const fs = require('fs');
+const http = require('http');
 
-//const data = fs.readFileSync('./data.txt','utf8');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const data = fs.readFile('./data.txt','utf8',function(err,data){
-   console.log(data);
-})
-//console.log (data)
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
