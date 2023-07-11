e820aed41476116087f31fb7d0ff0a8b18b4029a
kimchaneil <chaneil2@naver.com>
2023-07-12 04:08:19 +0900

    [ADD] express_helloworld

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const app = express();
 
-function commonmw(req,res,next){
-  console.log('commonmw')
-  next(new Error('error ouccered'));
-}
-function errormw(err,req,res,next){
-  console.log(err.message)
-  // error 처리 구문
-  next();
-}
+app.get('/',(req,res)=>{
+  res.send('hello world');
+})
 
-app.use(commonmw)
-app.use(errormw)
-app.listen(3000,function(){
-  console.log('Server is running');
+app.listen(3000,()=>{
+  console.log('server is running on 127.0.0.1:3000');
 })
