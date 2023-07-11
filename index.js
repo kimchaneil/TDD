52c42b4d19c5191cad294ecff894448729a14c5b
kimchaneil <chaneil2@naver.com>
2023-07-12 04:22:17 +0900

    [ADD] first_API

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
-
-app.get('/',(req,res)=>{
-  res.send('hello world');
+var users = [
+  {id:1, name: "alice"},
+  {id:2, name: "bek"},
+  {id:3, name: "chris"}
+]
+app.use(morgan('dev'));
+app.get('/users',(req,res)=>{
+  res.json(users);
 })
-
 app.listen(3000,()=>{
   console.log('server is running on 127.0.0.1:3000');
 })
