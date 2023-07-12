a7d371fcc9fc393f24abaf60902bbbfeefe808f2
kimchaneil <chaneil2@naver.com>
2023-07-12 21:38:19 +0900

    [ADD] user_add_API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 var users = [
   {id:1, name: "alice"},
@@ -7,6 +8,8 @@ var users = [
   {id:3, name: "chris"}
 ]
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); 
 app.get('/users',(req,res)=>{
   req.query.limit = req.query.limit || 10 ;
   //const limit = req.query.limit; // "2"
@@ -28,6 +31,17 @@ app.delete('/users/:id',(req,res)=>{
   if(Number.isNaN(id)) return res.status(400).end();
   users = users.filter(user=>user.id !== id);
   res.status(204).end();
+})
+app.post('/users',(req,res)=>{
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.filter(user => user.name === name).length;
+  if (isConflict) return res.status(409).end();
+  const id = Date.now();
+  const  user = {id,name};
+  users.push(user)
+  res.status(201).json(user);
 })
 app.listen(3000,()=>{
   console.log('server is running on 127.0.0.1:3000');
