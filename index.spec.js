0d994d55e51898ea07c9e4ba36fc74d2f1fe5f9d
kimchaneil <chaneil2@naver.com>
2023-07-12 15:00:14 +0900

    [ADD] first_userAPI

--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,15 @@
 const app = require('./index')
 const request = require('supertest')
-
+const should = require('should')
 describe('GET /users는',()=> {
-    it('.....',(done)=>{
-        request(app)
-            .get('/users')
-            .end((err,res)=>{
-                console.log(res.body);
-                done();
-            })
+    describe('성공시',()=>{
+        it('유저 객체를 담은 배열로 응답한 ',(done)=>{
+            request(app)
+                .get('/users')
+                .end((err,res)=>{
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                })
+        })
     })
 })
